cd42fffe2213bf1c2a2029e95183e91b51d6d9a2
Bishawjit1993 <Bishawjit1993@gmail.com>
2018-01-28 10:40:59 -0600

    change method

--- src/exercise/Methods.java
+++ src/exercise/Methods.java
@@ -3,5 +3,11 @@ package exercise;
 public class Methods {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		
+	}
+	public static String getUniqueCharacter(String text) {
+		String result = null;
+		
+		return result;
 	}
 }
